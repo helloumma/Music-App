af7224bcd0cd36423065b6a8e80616fd5ed05f95
ummagohil <umgo0510@gmail.com>
2023-01-15 16:54:11 +0000

    does something

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,40 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { Button, ButtonGroup } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
-// store the amount of times new is clicked
-// function for knowing the state/number 'new' is clicked and rendering the button with route
+// key press of anything other than a-g should be dealt with
 
 const Home: NextPage = () => {
 	const [noClicks, setNoClicks] = useState(0);
+	const [keyPress, setKeyPress] = useState(+new Date());
+	const [keyRelease, setKeyRelease] = useState();
+
+	const router = useRouter();
+
+	let date;
 
 	const updateClicks = () => {
-		// store the last click
+		// store the last click to preserve the button rendered
 		setNoClicks(noClicks + 1);
-		console.log(noClicks);
-		//clicks more than 4 redirect the user to home page
+		if (noClicks > 4) {
+			router.push("/");
+			setNoClicks(0);
+		}
+	};
+
+	const onKeUp = (e: React.KeyboardEvent) => {
+		console.log("key pressed date/time");
+		date = +new Date();
+		setKeyPress(date);
+	};
+
+	const onKeyDown = (e: React.KeyboardEvent) => {
+		console.log("key released date/time");
+		const diff = +new Date() - keyPress;
+		console.log(diff);
+		setKeyRelease(diff);
 	};
 
 	return (
@@ -28,19 +49,32 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className={styles.main}>
-				<Link href="/test">TEST</Link>
 				<Button colorScheme='red' onClick={updateClicks}>
 					New
 				</Button>
-				{noClicks}
-				{noClicks === 0 && <Button colorScheme='green'>one</Button>}
-				{noClicks === 1 && <Button colorScheme='green'>two</Button>}
-				{noClicks === 2 && <Button colorScheme='green'>three</Button>}
-				{noClicks === 3 && <Button colorScheme='green'>four</Button>}
+				{noClicks === 0 && (
+					<Link href="/">
+						<Button colorScheme='green'>one</Button>
+					</Link>
+				)}
+				{noClicks === 1 && (
+					<Link href="/page-two">
+						<Button colorScheme='green'>two</Button>
+					</Link>
+				)}
+				{noClicks === 2 && (
+					<Link href="/page-three">
+						<Button colorScheme='green'>three</Button>
+					</Link>
+				)}
+				{noClicks === 3 && (
+					<Link href="/page-four">
+						<Button colorScheme='green'>four</Button>
+					</Link>
+				)}
 				<div>
 					<h1>TO DO</h1>
-					<s>create new button</s>
-					<p>show buttons for new pages and program routes</p>
+					<p>show buttons for each page on new button click</p>
 					<p>
 						program the functionality of key press (for now show text and it can
 						be changed to an icon/notation after)
@@ -51,8 +85,11 @@ const Home: NextPage = () => {
 						steps between things like e/f etc etc)
 					</p>
 					<p>
-						ensure the data on each page is preserved - think about refreshes
+						quick fix: show the button click, figure out if it's upper or lower
+						case? and then add a flat or sharp
 					</p>
+					<input type="text" onKeyDown={onKeyDown} onKeyUp={onKeUp} />
+					{keyRelease}
 				</div>
 			</main>
 		</div>
