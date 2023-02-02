56b24dcbb82b8e9ded2781ec06a5e160d7bbbcfb
ummagohilpa <umma.gohil@paconsulting.com>
2023-02-02 10:30:54 +0000

    data passed between links

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,3 +1,9 @@
-const Details = () => <div>Details Page</div>;
+import { useRouter } from "next/router";
+
+const Details = () => {
+  const router = useRouter();
+  const id = router.query.id;
+  return <div>Details for {id}</div>;
+};
 
 export default Details;
