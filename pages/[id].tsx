732ca6e6160f8c9ae5d51401eb0325fe0f006155
ummagohilpa <umma.gohil@paconsulting.com>
2023-02-02 10:16:20 +0000

    dynamic routing

---
+++ pages/[id].tsx
@@ -0,0 +1,3 @@
+const Details = () => <div>Details Page</div>;
+
+export default Details;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,96 +10,116 @@ import { Input } from "@chakra-ui/react";
 
 // key press of anything other than a-g should be dealt with
 
+// need a useRef to store the amount of pages
+
 const Home: NextPage = () => {
-	const [noClicks, setNoClicks] = useState(0);
-	const [keyPress, setKeyPress] = useState(+new Date());
-	const [keyRelease, setKeyRelease] = useState();
+  const [noClicks, setNoClicks] = useState(0);
+  const [keyPress, setKeyPress] = useState(+new Date());
+  const [keyRelease, setKeyRelease] = useState();
 
-	const router = useRouter();
+  const router = useRouter();
 
-	let date;
+  let date;
 
-	const updateClicks = () => {
-		// store the last click to preserve the button rendered
-		setNoClicks(noClicks + 1);
-		if (noClicks > 4) {
-			router.push("/");
-			setNoClicks(0);
-		}
-	};
+  const updateClicks = () => {
+    // store the last click to preserve the button rendered
+    setNoClicks(noClicks + 1);
+    if (noClicks > 4) {
+      router.push("/");
+      setNoClicks(0);
+    }
+  };
 
-	const onKeUp = (e: React.KeyboardEvent) => {
-		console.log("key pressed date/time");
-		date = +new Date();
-		setKeyPress(date);
-	};
+  const onKeUp = (e: React.KeyboardEvent) => {
+    console.log("key pressed date/time");
+    date = +new Date();
+    setKeyPress(date);
+  };
 
-	const onKeyDown = (e: React.KeyboardEvent) => {
-		console.log("key released date/time");
-		const diff = +new Date() - keyPress;
-		console.log(diff);
-		setKeyRelease(diff / 1000);
-	};
+  const onKeyDown = (e: React.KeyboardEvent) => {
+    console.log("key released date/time");
+    const diff = +new Date() - keyPress;
+    console.log(diff);
+    setKeyRelease(diff / 1000);
+  };
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Music App</title>
-				<meta name="description" content="Music App" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  const dummyData: any = [
+    {
+      id: 1,
+      name: "test",
+    },
+    {
+      id: 2,
+      name: "test 2",
+    },
+  ];
 
-			<main className={styles.main}>
-				<Button colorScheme='red' onClick={updateClicks}>
-					New
-				</Button>
-				{noClicks === 0 && (
-					<Link href="/">
-						<Button colorScheme='green'>one</Button>
-					</Link>
-				)}
-				{noClicks === 1 && (
-					<Link href="/page-two">
-						<Button colorScheme='green'>two</Button>
-					</Link>
-				)}
-				{noClicks === 2 && (
-					<Link href="/page-three">
-						<Button colorScheme='green'>three</Button>
-					</Link>
-				)}
-				{noClicks === 3 && (
-					<Link href="/page-four">
-						<Button colorScheme='green'>four</Button>
-					</Link>
-				)}
-				<div>
-					<h1>TO DO</h1>
-					<p>show buttons for each page on new button click</p>
-					<p>
-						program the functionality of key press (for now show text and it can
-						be changed to an icon/notation after)
-					</p>
-					{/*Store the Date when the key is pressed. Store the Date when the key is released. Subtract the dates. */}
-					<p>
-						program the functionality of upper and lower case (and remember
-						steps between things like e/f etc etc)
-					</p>
-					<p>
-						quick fix: show the button click, figure out if it's upper or lower
-						case? and then add a flat or sharp
-					</p>
-					{/* the input needs to be a component that can be used in all pages */}
-					{/* deal with the upper and lower case letters */}
-					{/* deal with only a-g */}
-					{/* program functionality of mappying the time to a type of note */}
-					{/* show the note either normal, flat, sharp and the type of note */}
-					<Input type="text" onKeyDown={onKeyDown} onKeyUp={onKeUp} />
-					{keyRelease}
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Music App</title>
+        <meta name="description" content="Music App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Button colorScheme="red" onClick={updateClicks}>
+          New
+        </Button>
+        {dummyData.map((a: any) => (
+          <Link key={a.id} href={`/${a.id}`}>
+            {a.name}
+          </Link>
+        ))}
+
+        {/* map over the number of clicks and create a button for each  */}
+        {noClicks === 0 && (
+          <Link href="/">
+            <Button colorScheme="green">one</Button>
+          </Link>
+        )}
+        {noClicks === 1 && (
+          <Link href="/page-two">
+            <Button colorScheme="green">two</Button>
+          </Link>
+        )}
+        {noClicks === 2 && (
+          <Link href="/page-three">
+            <Button colorScheme="green">three</Button>
+          </Link>
+        )}
+        {noClicks === 3 && (
+          <Link href="/page-four">
+            <Button colorScheme="green">four</Button>
+          </Link>
+        )}
+        <div>
+          <h1>TO DO</h1>
+          <p>show buttons for each page on new button click</p>
+          <p>
+            program the functionality of key press (for now show text and it can
+            be changed to an icon/notation after)
+          </p>
+          {/*Store the Date when the key is pressed. Store the Date when the key is released. Subtract the dates. */}
+          <p>
+            program the functionality of upper and lower case (and remember
+            steps between things like e/f etc etc)
+          </p>
+          <p>
+            quick fix: show the button click, figure out if it's upper or lower
+            case? and then add a flat or sharp
+          </p>
+          {/* the input needs to be a component that can be used in all pages */}
+          {/* deal with the upper and lower case letters */}
+          {/* deal with only a-g */}
+          {/* program functionality of mappying the time to a type of note */}
+          {/* show the note either normal, flat, sharp and the type of note */}
+          <Input type="text" onKeyDown={onKeyDown} onKeyUp={onKeUp} />
+          {keyRelease}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
