36fcf3e085701375370199bbc85dee3cfb4d954e
ummagohil <umgo0510@gmail.com>
2023-02-24 17:09:31 +0000

    current

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,331 +1,247 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import {
-  Button,
-  ButtonGroup,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	useMenuState,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { useRouter } from "next/router";
 import { Input } from "@chakra-ui/react";
-import { DummyData, DataContainer } from "../types";
-import { Flex, Spacer } from "@chakra-ui/react";
-
-// key press of anything other than a-g should be dealt with
-
-// DO THE ABOVE TOMORROW
-
-type Props = {
-  data: DummyData;
-};
-
-
-
+import { DataContainer } from "../types";
+import { Flex } from "@chakra-ui/react";
 
 /*
- const dummyData: { id: number; name: string }[] = [
-    {
-      id: 1,
-      name: "test",
-    },
-    {
-      id: 2,
-      name: "test 2",
-    },
-  ]; */
 
-/*
-export interface DummyData {
+  TO DO [24th feb - 26th feb]
+
+  24th Feb
+  * program user selection of dropdown menu + the total keys in the input box + the total time => add an alert box of correct or wrong
+  * program constrains for only entering notes for one bar in the time signature
+  
+  25th Feb
+  * program click handler on the buttons
+  * program user selection of dropdown menu + total time => input box
+  * program reusable component which can be used on the dynamic pages
+  * add a back button on dynamic pages
+  
+  26th Feb
+  * merge into main
+  * deploy a small version of the app
+  * update the read me 
+*/
+
+/*export type tryingData = DataContainer<{
 	id: number;
 	name: string;
-}
- */
+}>;*/
 
-export type tryingData = DataContainer<{
-  id: number,
-  name: string
-}>
+type dropDown = {
+	value: string;
+};
 
-/*const test:tryingData = {
-  data: [{
-    id: 0,
-    name: "test1"
-  }, ]
-}*/
+const Home: NextPage = () => {
+	const [keyPress, setKeyPress] = useState<number>(+new Date());
+	const [keyRelease, setKeyRelease] = useState<number>(0);
+	const [count, setCount] = useState<number>(0);
+	const [inputSelect, setInputSelect] = useState<boolean>(false);
+	const [buttonSelect, setButtonSelect] = useState<boolean>(false);
+	const [value, setNewVal] = useState();
 
-/*
- const dummyData: { id: number; name: string }[] = [
-    {
-      id: 1,
-      name: "test",
-    },
-    {
-      id: 2,
-      name: "test 2",
-    },
-  ];*/
+	let date: number;
 
-//console.log(test)
-//console.log(test?.data.map((a: { id: number; name: string }) => a.id +  a.name))
+	const onKeUp = (e: React.KeyboardEvent) => {
+		//console.log("key pressed date/time");
+		date = +new Date();
+		setKeyPress(date);
+	};
 
-const test: tryingData = {
-  data: {
-    id: 0,
-    name: "test"
-  }
-}
+	const onKeyDown = (e: React.KeyboardEvent) => {
+		//console.log("key released date/time");
+		const diff: number = +new Date() - keyPress;
+		//console.log(diff);
+		setKeyRelease(diff / 1000);
+		//console.log(e.key);
+		if (e.key.match(/[h-z]/gi)) alert("This is not a musical note.");
+		// figure out how to disable/look into focus
+	};
 
+	const handleClick = () => {
+		setCount(count + 1);
+	};
 
+	// TO DO: you need to use types/interface here
+	const buttons = Array(count)
+		.fill(count)
+		.map((_: number, i: number) => (
+			<Link key={i} href={`/${i}`}>
+				<Button key={i} colorScheme="green">
+					Button {i + 1}
+				</Button>
+			</Link>
+		));
 
-const Home: NextPage<Props> = (Props) => {
-  const [noClicks, setNoClicks] = useState(0);
-  const [keyPress, setKeyPress] = useState(+new Date());
-  const [keyRelease, setKeyRelease] = useState(0);
-  const [count, setCount] = useState(0);
-	const [inputSelect, setInputSelect] = useState(false)
-	const [buttonSelect, setButtonSelect] = useState(false)
-
-  const router = useRouter();
-
-  let date: number;
-
-  const updateClicks = () => {
-    // store the last click to preserve the button rendered
-    setNoClicks(noClicks + 1);
-    if (noClicks > 4) {
-      router.push("/");
-      setNoClicks(0);
-    }
-  };
-
-  const onKeUp = (e: React.KeyboardEvent) => {
-    console.log("key pressed date/time");
-    date = +new Date();
-    setKeyPress(date);
-  };
-
-  const onKeyDown = (e: React.KeyboardEvent) => {
-    console.log("key released date/time");
-    const diff: number = +new Date() - keyPress;
-    console.log(diff);
-    setKeyRelease(diff / 1000);
-    console.log(e.key);
-    // if e.key is not a-g alert box
-    if (e.key.match(/[h-z]/gi)) alert("This is not a musical note.");
-    // figure out how to disable/look into focus
-  };
-
-  const dummyData: { id: number; name: string }[] = [
-    {
-      id: 1,
-      name: "test",
-    },
-    {
-      id: 2,
-      name: "test 2",
-    },
-  ];
-
-  //console.log(myData)
-
-  //console.log(Props);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  const buttons = Array(count)
-    .fill(count)
-    .map((_, i: number) => (
-      <Link key={i} href={`/${i}`}>
-        <Button key={i} colorScheme="green">
-          Button {i + 1}
-        </Button>
-      </Link>
-    ));
-
+	// TO DO: Types/interface
 	const inputClick = () => {
 		//e.preventdefault()
-		setInputSelect(true)
-		setButtonSelect(false)
-	}
+		setInputSelect(true);
+		setButtonSelect(false);
+	};
 
+	// TO DO: Types/interface
 	const buttonClick = () => {
-		setInputSelect(false)
-		setButtonSelect(true)
+		setInputSelect(false);
+		setButtonSelect(true);
+	};
 
-		
-	}
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Music App</title>
-        <meta name="description" content="Music App" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const handleDropDown = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNewVal(e.target.value);
+	};
 
-      <main className={styles.main}>
-			<h1 className="app-title">music app</h1>
-			<p>This is a small music app to gain a better understanding of time signatures and types of music notes.</p>
-			
-			
-		<div className="main-instructions">
-			{inputSelect || buttonSelect ? (<ol>
-					<li>Select a time signature from the menu</li>
-					{inputSelect && <li>Hold down a letter between A to G into the input box</li> }
-					{buttonSelect && <li>Click and hold on buttons letter between A to G into the input box</li>}
-					<li>The type of note will be shown on screen </li>
-					<li>You may create new pages to experiment with different notes and signatures</li>
-					<li>You cannot input values outside of A to G</li>
-				</ol>) : "Make a selection below to view instructions."}
-				
-		</div>
-		<Flex direction={"row"}>
-			<Button colorScheme="blue" onClick={inputClick}>Input Box</Button>
-			<Button colorScheme="blue" onClick={buttonClick}>Buttons</Button>
+	const computeNotes = () => {
+		console.log(
+			"check the amount of notes entered (time wise) add up to time signature here",
+		);
+
+		console.log(
+			"reminder: you are comparing time signature bpm to amount total seconds of all notes",
+		);
+
+		// you need the total vals from the state for pressDown
+
+		// you need the total vals for button click
+
+		/*
+      calcs
+
+      bpm: 50
+      1 beat => 1.2 second
+      1 bar (3/4) => 3.6 seconds 
+      1 bar (2/4) => 2.4 seconds
+      1 bar (4/4) => 4.8 seconds 
+     */
+	};
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Music App</title>
+				<meta name="description" content="Music App" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<h1 className="app-title">music app</h1>
+				<p>
+					This is a small music app to gain a better understanding of time
+					signatures and types of music notes.
+				</p>
+				<Flex direction={"row"}>
+					<Button colorScheme="blue" onClick={inputClick}>
+						Input Box
+					</Button>
+					<Button colorScheme="blue" onClick={buttonClick}>
+						Buttons
+					</Button>
 				</Flex>
-        <div className="main-section">
-          <Flex direction={"row"} justifyContent={"space-between"}>
-            <Button colorScheme="red" onClick={handleClick}>
-              New
-            </Button>
-            <Menu>
-              <MenuButton as={Button}>Time Signature [TO DO]</MenuButton>
-              <MenuList>
-                <MenuItem>2/4</MenuItem>
-                <MenuItem>3/4</MenuItem>
-                <MenuItem>4/4</MenuItem>
-                <MenuItem>6/8</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
+				<div className="main-instructions">
+					{inputSelect || buttonSelect ? (
+						<ol>
+							<li>Select a time signature from the menu</li>
+							{inputSelect && (
+								<li>Hold down a letter between A to G into the input box</li>
+							)}
+							{buttonSelect && (
+								<li>
+									Click and hold on buttons letter between A to G into the input
+									box
+								</li>
+							)}
+							<li>The type of note will be shown on screen </li>
+							<li>
+								You may create new pages to experiment with different notes and
+								signatures
+							</li>
+							<li>You cannot input values outside of A to G</li>
+						</ol>
+					) : (
+						"Make a selection above to view instructions."
+					)}
+				</div>
 
-          <Flex key={count} direction={"row"} justifyContent="center">
-            {buttons}
-          </Flex>
-          {/*<p>
-          Add a dropdown menu to select the time signature and then program the
-          timing of each note!!!! beats per min etc.
-	</p>*/}
+				<div className="main-section">
+					<Flex direction={"row"} justifyContent={"space-between"}>
+						<Button colorScheme="red" onClick={handleClick}>
+							New
+						</Button>
+						<Menu>
+							{/* TO DO: REFACTOR  */}
+							<MenuButton as={Button}>Time Signature</MenuButton>
+							<MenuList>
+								<MenuItem onClick={handleDropDown} value="2/4">
+									2/4
+								</MenuItem>
+								<MenuItem onClick={handleDropDown} value="3/4">
+									3/4
+								</MenuItem>
+								<MenuItem onClick={handleDropDown} value="4/4">
+									4/4
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					</Flex>
+					{value}
+					<Flex key={count} direction={"row"} justifyContent="center">
+						{buttons}
+					</Flex>
 
-          {/*<Button colorScheme="red" onClick={updateClicks}>
-          New
-        </Button>
-        {dummyData.map((a: { id: number; name: string }) => (
-          <Link key={a.id} href={`/${a.id}`}>
-            {a.name}
-          </Link>
-				))}*/}
-      
-
-          {/* map over the number of clicks and create a button for each  */}
-
-          {/*Store the Date when the key is pressed. Store the Date when the key is released. Subtract the dates. */}
-          <h1>CALCULATIONS FOR TIME SIGNATURES</h1>
-
-					<p>2/4 => two crochets per semibreve</p>
-					<p>3/4 => three crochet per semibreve</p>
-					<p>4/4 => one crochet per semibreve</p>
-
-          <p>
-            program the functionality of upper and lower case (and remember
-            steps between things like e/f etc etc)
-          </p>
-
-					<p>Potentially have an option to have buttons A to G, with a click handler of timing of click up and off</p>
-					<p>Idea: Create an option/button which enables the user to select between input and button </p>
-					<p>Maybe: Get the user to press down on a series of notes to make up the time signature</p>
-          {/* the input needs to be a component that can be used in all pages */}
-          {/* deal with the upper and lower case letters */}
-          {/* program functionality of mapping the time to a type of note */}
-          {/* show the note either normal, flat, sharp and the type of note */}
-          <p>show and hide buttons/input here</p>
+					<p>
+						Reminder: Only enter or press down notes for ONE bar within the time
+						signature. BPM is set to 50.
+					</p>
 					<Flex direction={"row"} justifyContent="center">
 						{inputSelect && (
 							<Input type="text" onKeyDown={onKeyDown} onKeyUp={onKeUp} />
 						)}
-            {buttonSelect && (
+						{buttonSelect && (
 							<>
-							<Button>A</Button>
-							<Button>B</Button>
-							<Button>C</Button>
-							<Button>D</Button>
-							<Button>E</Button>
-							<Button>F</Button>
-							<Button>G</Button>
+								<Button>A</Button>
+								<Button>B</Button>
+								<Button>C</Button>
+								<Button>D</Button>
+								<Button>E</Button>
+								<Button>F</Button>
+								<Button>G</Button>
 							</>
-							
-
 						)}
-            {/*<Button colorScheme="blue">Submit</Button>*/}
-          </Flex>
+					</Flex>
 
-          {/*Use submit instead of entering???*/}
-          {keyRelease}
-          {keyRelease < 0
-            ? ""
-            : keyRelease <= 0.25
-            ? "semi-quaver"
-              ? keyRelease <= 0.5
-              : "quaver"
-              ? keyRelease <= 1
-              : "crotchet"
-              ? keyRelease <= 2
-              : "minimum"
-              ? keyRelease <= 4
-              : "semibreve"
-            : ""}
-          {/*keyRelease > 0 && keyRelease <= 0.25 ? "semi-quaver" : "no"*/}
-          {keyRelease >= 0.25 && "semi-quaver"}
-          {keyRelease >= 0.5 && "quaver"}
-          {keyRelease >= 1 && "crotchet"}
-          {keyRelease >= 2 && "minimum"}
-          {keyRelease >= 4 && "semibreve"}
-          {/*<h1>TO DO</h1>
-         <p>Show the type of note and the case</p>
-          <p>
-            Program upper and lower case (for step up or down) and then show the
-            type of note
-          </p>
-          <p>
-            Maybe: detect the key pressed and why the note shown (oh that's an
-            upper C so it's c#)
-						</p>*/}
-        </div>
-      </main>
-    </div>
-  );
+					{keyRelease}
+					{/*keyRelease < 0
+						? ""
+						: keyRelease <= 0.25
+						? "semi-quaver"
+							? keyRelease <= 0.5
+							: "quaver"
+							? keyRelease <= 1
+							: "crotchet"
+							? keyRelease <= 2
+							: "minimum"
+							? keyRelease <= 4
+							: "semibreve"
+            : ""*/}
+					{/*keyRelease >= 0.25 && "semi-quaver"*/}
+					{/*keyRelease >= 0.5 && "quaver"*/}
+					{/*keyRelease >= 1 && "crotchet"*/}
+					{/*keyRelease >= 2 && "minimum"*/}
+					{/*keyRelease >= 4 && "semibreve"*/}
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
-/* {noClicks === 0 && (
-          <Link href="/">
-            <Button colorScheme="green">one</Button>
-          </Link>
-        )}
-        {noClicks === 1 && (
-          <Link href="/page-two">
-            <Button colorScheme="green">two</Button>
-          </Link>
-        )}
-        {noClicks === 2 && (
-          <Link href="/page-three">
-            <Button colorScheme="green">three</Button>
-          </Link>
-        )}
-        {noClicks === 3 && (
-          <Link href="/page-four">
-            <Button colorScheme="green">four</Button>
-          </Link>
-        )}
- */
